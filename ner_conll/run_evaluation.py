c0801a89c66253b3b99a790f45d981c5f2852068
edchengg <yangc3@student.unimelb.edu.au>
2022-04-09 20:59:02 -0400

    ed

--- ner_conll/run_evaluation.py
+++ ner_conll/run_evaluation.py
@@ -4,7 +4,7 @@ import os
 print('Loading ckpt and reproduce results for Table 3 NER CoNLL...')
 
 for lang in ['de', 'es', 'nl', 'zh']:
-    if os.path.exists('{}.ckpt'.format('pos-{}-lms'.format(lang))):
+    if os.path.exists('{}.ckpt'.format('ner-{}-lms'.format(lang))):
         pass
     else:
         print('ckpt not exists')
