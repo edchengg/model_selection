63f9e10e63b0341832dd8b08da0ca1fb07065df8
edchengg <yangc3@student.unimelb.edu.au>
2022-04-09 20:47:17 -0400

    ed

--- bash.sh
+++ bash.sh
@@ -2,26 +2,31 @@
 
 # Baseline
 #pos
+echo "POS - Table 3"
 cd pos/lms_table3
 python baseline.py
 python baseline_bg-sv.py
 python run_evaluation.py
 #qa
+echo "QA - Table 3"
 cd ../../qa
 python baseline.py
 python run_evaluation.py
 #conll
+echo "NER CoNLL - Table 3"
 cd ..
 cd ..
 cd ner_conll
 python baseline.py
 python run_evaluation.py
 #low-resource ner
+echo "NER Low Resource - Table 3"
 cd ..
 cd ner_low_resource/lms_table3
 python baseline.py
 python evaluation.py
 #pos-multilingual
+echo "POS Multilingual - Table 4"
 cd ..
 cd ..
 cd pos/lms_table4
